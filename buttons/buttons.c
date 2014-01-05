e97728dde33b7078c62500c94539246cc94730bd
David Hovemeyer <david.hovemeyer@gmail.com>
2014-01-05 12:24:25 -0500

    code cleanups

--- buttons/buttons.c
+++ buttons/buttons.c
@@ -17,7 +17,7 @@
 // along with CarPi.  If not, see <http://www.gnu.org/licenses/>.
 
 // Use attiny2313 to debounce 6 pushbuttons
-// The buttons should connect to ground
+// The buttons should connect to ground when pressed
 
 #define F_CPU 1000000 // use internal oscillator, set to 1 MHz
 #include <stdint.h>
@@ -44,10 +44,18 @@
 // Button 5 - PD2
 // Button 6 - PD3
 
-uint8_t cur = 0x3F;      // current input values
-uint8_t last = 0x3F;     // last input values
-uint8_t count[6];        // count of how many polls buttons have had their current values
-uint8_t out = 0x3F;      // current output values
+// Bit masks for output pins on ports A and D
+#define PORTA_OUTPUT_MASK (_BV(PA0)|_BV(PA1))
+#define PORTD_OUTPUT_MASK (_BV(PD0)|_BV(PD1)|_BV(PD2)|_BV(PD3))
+
+// Bit masks for input pins on ports B and D
+#define PORTB_INPUT_MASK (_BV(PB0)|_BV(PB1)|_BV(PB2)|_BV(PB3)|_BV(PB4))
+#define PORTD_INPUT_MASK _BV(PD6)
+
+uint8_t g_cur = 0x3F;      // current input values
+uint8_t g_last = 0x3F;     // last input values
+uint8_t g_count[6];        // count of how many polls buttons have had their current values
+uint8_t g_out = 0x3F;      // current output values
 
 void read_inputs(void);
 void update_counts(void);
@@ -59,15 +67,15 @@ int main(void)
 	DDRB = 0;
 
 	// Enable PA0-1, PD0-3 as outputs
-	DDRA = (_BV(PA0) | _BV(PA1));
-	DDRD = (_BV(PD0) | _BV(PD1) | _BV(PD2) | _BV(PD3));
+	DDRA = PORTA_OUTPUT_MASK;
+	DDRD = PORTD_OUTPUT_MASK;
 #ifndef NDEBUG
 	DDRD |= _BV(PD4);
 #endif
 
 	// Enable internal pull-up resistors on PB0-4, PD6
-	PORTB = (_BV(PB0) | _BV(PB1) | _BV(PB2) | _BV(PB3) | _BV(PB4));
-	PORTD = _BV(PD6);
+	PORTB = PORTB_INPUT_MASK;
+	PORTD = PORTD_INPUT_MASK;
 
 #ifndef NDEBUG
 	uint8_t count = 0;
@@ -76,9 +84,8 @@ int main(void)
 	for (;;) {
 		read_inputs();
 		update_counts();
-		//out = cur; // no debouncing
 		write_outputs();
-		last = cur;
+		g_last = g_cur;
 		_delay_ms(1);
 
 #ifndef NDEBUG
@@ -95,59 +102,59 @@ void read_inputs(void)
 	// least significant bit
 	uint8_t inb = PINB;
 	uint8_t ind = PIND;
-	cur = 0;
+	g_cur = 0;
 
 	// Read input for button 6
-	cur |= ((inb & _BV(PB4)) != 0); // button 6
-	cur <<= 1;
+	g_cur |= ((inb & _BV(PB4)) != 0); // button 6
+	g_cur <<= 1;
 
 	// Read input for buttons 5-1
-	cur |= ((inb & _BV(PB3)) != 0); // button 5
-	cur <<= 1;
-	cur |= ((inb & _BV(PB2)) != 0); // button 4
-	cur <<= 1;
-	cur |= ((inb & _BV(PB1)) != 0); // button 3
-	cur <<= 1;
-	cur |= ((inb & _BV(PB0)) != 0); // button 2
-	cur <<= 1;
-	cur |= ((ind & _BV(PD6)) != 0); // button 1
+	g_cur |= ((inb & _BV(PB3)) != 0); // button 5
+	g_cur <<= 1;
+	g_cur |= ((inb & _BV(PB2)) != 0); // button 4
+	g_cur <<= 1;
+	g_cur |= ((inb & _BV(PB1)) != 0); // button 3
+	g_cur <<= 1;
+	g_cur |= ((inb & _BV(PB0)) != 0); // button 2
+	g_cur <<= 1;
+	g_cur |= ((ind & _BV(PD6)) != 0); // button 1
 }
 
 void update_counts(void)
 {
 	// Update counts, allow outputs to change if count is
 	// sufficiently high
-	uint8_t nextout = out;
+	uint8_t nextout = g_out;
 	for (uint8_t i = 0, mask = 1; i < 6; i++, mask <<= 1) {
-		if ((cur & mask) == (last & mask)) {
+		if ((g_cur & mask) == (g_last & mask)) {
 			// Button input value is same as last poll: increment count
-			if (count[i] < 255) { count[i]++; }
+			if (g_count[i] < 255) { g_count[i]++; }
 		} else {
 			// Button input value is different from last poll:
 			// reset count to 0
-			count[i] = 0;
+			g_count[i] = 0;
 		}
 
 		// Allow output to change if count is sufficiently high
-		if (count[i] > DEBOUNCE_TIME_MS && (cur & mask) != (out & mask)) {
+		if (g_count[i] > DEBOUNCE_TIME_MS && (g_cur & mask) != (g_out & mask)) {
 			nextout ^= mask;
 		}
 	}
 
-	out = nextout;
+	g_out = nextout;
 }
 
 void write_outputs(void)
 {
 	// Get current port A/D values, but leaving 0s for the bits
 	// corresponding to the button outputs we want to generate
-	uint8_t outa = PORTA & ~(_BV(PA0) | _BV(PA1));
-	uint8_t outd = PORTD & ~(_BV(PD0) | _BV(PD1) | _BV(PD2) | _BV(PD3));
+	uint8_t outa = PORTA & ~PORTA_OUTPUT_MASK;
+	uint8_t outd = PORTD & ~PORTD_OUTPUT_MASK;
 
 	// Set button output bits in port A/D as appropriate.
 	// Note that the button inputs are 0=pressed, but we generate
 	// the debounced outputs as 1=pressed.
-	uint8_t genout = out;
+	uint8_t genout = g_out;
 	if ((genout & 1) == 0) { outd |= _BV(PD0); }
 	genout >>= 1;
 	if ((genout & 1) == 0) { outd |= _BV(PD1); }
