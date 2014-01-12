3c493fa4b3e440b3276b1eb8ba902397e78d6915
David Hovemeyer <david.hovemeyer@gmail.com>
2014-01-12 20:52:45 +0000

    added copyright header

--- CarPi/button_input_reader_thread.cpp
+++ CarPi/button_input_reader_thread.cpp
@@ -1,3 +1,21 @@
+// CarPi - Raspberry Pi car entertainment system
+// Copyright (c) 2013,2014 David H. Hovemeyer <david.hovemeyer@gmail.com>
+
+// This file is part of CarPi.
+// 
+// CarPi is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// CarPi is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with CarPi.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <unistd.h>
 #include "gpio_pin.h"
 #include "event.h"
